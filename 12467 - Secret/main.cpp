840c7261a7f85709b97e71310d61ef6f9cd0e2cd
Patrick Vares <pvrs12@gmail.com>
2014-10-27 23:52:23 -0400

    code works for small strings

--- 12467 - Secret/main.cpp
+++ 12467 - Secret/main.cpp
@@ -8,34 +8,17 @@ using namespace std;
 
 typedef vector<int> trie;
 
-string* reverse(string* s, int len) {
+string* reverse(string* s, unsigned len) {
 	string* st = new string();
-	for (int i = len; i >= 0; --i) {
+	for (unsigned i = len; i >= 0; --i) {
 		(*st) += (*s)[i];
 	}
 	return st;
 }
 
-struct compare {
-	compare(string* s, int ordering = 0) :
-			s(s), ordering(ordering) {
-	}
-	string* s;
-	int ordering;
-
-	bool operator()(int a, int b) const {
-		if (ordering == 0)
-			//alphabetical
-			return *(s->c_str() + a) < *(s->c_str() + b);
-		//strcmp(s->c_str() + a, s->c_str() + b) < 0;
-		else
-			//length (greatest to least)
-			return strlen(s->c_str() + a) > strlen(s->c_str() + b);
-	}
-};
-
-int stringMatching(const string& s, const trie& sa, const char* sub, int len) {
-	int lo = 0, hi = len - 1, mid = lo;
+int stringMatching(const string& s, const trie& sa, const char* sub,
+		unsigned len) {
+	int lo = 0, hi = s.length() - 1, mid;
 	while (lo < hi) {
 		mid = (lo + hi) / 2;
 		int res = strncmp(s.c_str() + sa[mid], sub, len);
@@ -51,6 +34,57 @@ int stringMatching(const string& s, const trie& sa, const char* sub, int len) {
 	return lo;
 }
 
+void countingSort(int k, string& s, vector<char>& RA, trie& SA, trie& tempSA) {
+	unsigned maxi = max((unsigned) 300, (unsigned) s.length());
+	vector<int> c(maxi + 10, 0);
+	for (unsigned i = 0; i < s.length(); ++i) {
+		c[i + k < s.length() ? RA[i + k] : 0]++;
+	}
+	int sum = 0;
+	for (unsigned i = 0; i < maxi; ++i) {
+		int t = c[i];
+		c[i] = sum;
+		sum += t;
+	}
+	for (unsigned i = 0; i < s.length(); ++i) {
+		tempSA[c[SA[i] + k < (int) s.length() ? RA[SA[i] + k] : 0]++] = SA[i];
+	}
+	for (unsigned i = 0; i < s.length(); ++i) {
+		SA[i] = tempSA[i];
+	}
+}
+
+trie* constructSA(string* s) {
+	vector<char> RA(s->length()), tempRA(s->length());
+	trie* SA = new trie(s->length()), tempSA(s->length());
+	for (unsigned i = 0; i < s->length(); ++i) {
+		RA[i] = (*s)[i];
+		tempRA[i] = (*s)[i];
+	}
+	for (unsigned i = 0; i < s->length(); ++i) {
+		(*SA)[i] = i;
+	}
+	for (unsigned k = 1; k < s->length(); k <<= 1) {
+		countingSort(k, *s, RA, *SA, tempSA);
+		countingSort(0, *s, RA, *SA, tempSA);
+		unsigned r = 0;
+		tempRA[(*SA)[0]] = 0;
+		for (unsigned i = 1; i < s->length(); ++i) {
+			tempRA[(*SA)[i]] =
+					(RA[(*SA)[i]] == RA[(*SA)[i - 1]]
+							&& RA[(*SA)[i] + k] == RA[(*SA)[i - 1] + k]) ?
+							r : ++r;
+		}
+		for (unsigned i = 0; i < s->length(); ++i) {
+			RA[i] = tempRA[i];
+		}
+		if (RA[(*SA)[s->length() - 1]] == (int) s->length() - 1) {
+			break;
+		}
+	}
+	return SA;
+}
+
 int main() {
 	int T;
 	cin >> T;
@@ -59,36 +93,28 @@ int main() {
 		cin >> *s;
 
 		//build a suffix array of s and reversed s
-		//this step is O(n^2 log n)
-		trie SAs;
-		trie SAr;
-		//O(n)
-		for (unsigned i = 0; i < s->size(); ++i) {
-			SAs.push_back(i);
-			SAr.push_back(i);
-		}
-		//O(n log(n))
-		sort(SAs.begin(), SAs.end(), compare(s, 0));
+		//this step is O(n log n)
+		cout << "building trie" << endl;
+		trie* SAs = constructSA(s);
+		cout << "done building trie" << endl;
 
 		string* reverseS = reverse(s, s->length() - 1);
-		//O(n log(n))
-		sort(SAr.begin(), SAr.end(), compare(reverseS, 1));
-//		for (int i = 0; i < SAs.size(); ++i) {
-//			cout << s->c_str() + SAs[i] << "\t" << reverseS->c_str() + SAr[i]
-//					<< endl;
+
+//		for (unsigned i = 0; i < SAs->size(); ++i) {
+//			cout << s->c_str() + (*SAs)[i] << endl;
 //		}
 
-		//O(n * n log n)
-//		for (unsigned i = 0; i < SAr.size(); ++i) {
-//			int a = stringMatching(*s, SAs, reverseS->c_str() + SAr[i],
-//					reverseS->length() - SAr[i]);
-//			if (a != -1) {
-//				cout << reverseS->c_str() + SAr[i] << endl;
-//				break;
-//			}
-//		}
+		for (unsigned i = 0; i < reverseS->length(); ++i) {
+			int a = stringMatching(*s, *SAs, reverseS->c_str() + i,
+					reverseS->length() - i);
+			if (a != -1) {
+				cout << reverseS->c_str() + i << endl;
+				break;
+			}
+		}
 
 		delete s;
+		delete SAs;
 		delete reverseS;
 	}
 	return 0;
