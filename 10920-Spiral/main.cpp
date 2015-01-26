56cfed9c4182494defbd7488d5b2a42c27da4b64
pvrs12 <pvrs12@gmail.com>
2015-01-26 11:46:08 -0500

    This is Spiral Tap. It goes to 11

---
+++ 10920-Spiral/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+	while (true) {
+		int sz, p;
+		cin >> sz >> p;
+		if (!sz && !p) {
+			break;
+		}
+		//top right is square
+
+
+	}
+
+}
