7eadd19abd6f25e067fc8b23533f6072a68930ca
pvrs12 <pvrs12@gmail.com>
2015-01-27 11:59:13 -0500

    asdf

--- 10718-BitMask/main.cpp
+++ 10718-BitMask/main.cpp
@@ -22,25 +22,63 @@ int main() {
 		}
 
 		const bitset<32> N(Ni), L(Li), U(Ui);
-		bitset<32> M(Li);
-		cout<<N<<endl;
-		//go through each bit
-		//left to right, turn on and check if L <= M (don't need to check U because M starts equal to U)
-		//for biggest N | M, turn on M if !N
-		for (int i = 0; i < 32; ++i) {
-			if (M[i] == N[i]) {
-				//if they're the same, flip M
-				M[i].flip();
-				if (M > U) {
-					//if that makes it bigger than U, un-flip
-					M[i].flip();
-				} else if (M < L) {
-					// if it makes it less than L, un-flip
+		bitset<32> M(0);
+		//cout << N << endl;
+
+		bool less = false;
+		for (int i = 32; i >= 0; --i) {
+			//go through comparing with U and N to build M
+			if (!U[i] && N[i]) {
+				//U[i]==0
+				//N[i]==1
+				//do nothing
+			} else if (U[i] && N[i]) {
+				//U[i]==1
+				//N[i]==1
+				//i am now less than U no matter what I do in M
+				less = true;
+			} else if (U[i] && !N[i]) {
+				//U[i]==1
+				//N[i]==0
+				//do nothing
+				//because it makes sense to leave M at 0 for lower values
+				//we are now less than U
+				less = true;
+			} else if (!U[i] && !N[i]) {
+				//U[i]==0
+				//N[i]==0
+				//have to check if it is definitely less, otherwise we can't flip
+				if (less) {
 					M[i].flip();
 				}
 			}
 		}
 
+		for (int i = 32; i >= 0; --i) {
+			//compare M with L to fix M to be greater than L
+			if (!L[i] && M[i]) {
+				//L[i]==0
+				//M[i]==1
+				//too big must flip bit
+				M[i].flip();
+			} else if (L[i] && M[i]) {
+				//L[i]==1
+				//M[i]==1
+				//same size
+				//do nothing
+				//no need to check for a flip here because we minimized before
+			} else if (!L[i] && !M[i]) {
+				//L[i]==0
+				//M[i]==0
+				//same size
+				//do nothing
+			} else if (L[i] && !M[i]) {
+				//L[i]==1
+				//M[i]==0
+				//i am definitely smaller
+				break;
+			}
+		}
 		cout << M.to_ulong() << endl;
 	}
 }
