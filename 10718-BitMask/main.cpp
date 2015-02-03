ab9382d4b355e3b6675738c91f8449da3ce951ce
pvrs12 <pvrs12@gmail.com>
2015-02-03 16:15:21 -0500

    a

--- 10718-BitMask/main.cpp
+++ 10718-BitMask/main.cpp
@@ -1,84 +1,30 @@
-#include <iostream>
-#include <bitset>
-#include <climits>
-
-using namespace std;
-
-bool operator<(bitset<32> a, bitset<32> b) {
-	return a.to_ulong() < b.to_ulong();
+#include <stdio.h>
+typedef long long int ll;
+ll solve(ll L, ll U, ll N) {
+    ll res = 0;
+    int gt = 0;
+    int i;
+    for (i = 31; i >= 0; i--) {
+        if (gt) {
+            if ((U & (1LL << i)) && !(N & (1LL << i)))
+                res |= 1LL << i;
+            else if ((U & (1LL << i)))
+                U |= (1LL << i) - 1;
+        } else {
+            if ((U & (1LL << i)) && (L & (1L << i)))
+                res |= 1LL << i;
+            else if ((U & (1LL << i)) && !(N & (1LL << i))) {
+                res |= 1LL << i;
+                gt = 1;
+            } else if ((U & (1LL << i)))
+                U |= (1LL << i) - 1;
+        }
+    }
+    return res;
 }
-
-bool operator>(bitset<32> a, bitset<32> b) {
-	return a.to_ulong() > b.to_ulong();
-}
-
 int main() {
-	while (true) {
-		long long Ni, Li, Ui;
-		Ni = Li = Ui = -1;
-		cin >> Ni >> Li >> Ui;
-		if (Ni == -1 && Li == -1 && Ui == -1) {
-			break;
-		}
-
-		const bitset<32> N(Ni), L(Li), U(Ui);
-		bitset<32> M(0);
-		//cout << N << endl;
-
-		bool less = false;
-		for (int i = 32; i >= 0; --i) {
-			//go through comparing with U and N to build M
-			if (!U[i] && N[i]) {
-				//U[i]==0
-				//N[i]==1
-				//do nothing
-			} else if (U[i] && N[i]) {
-				//U[i]==1
-				//N[i]==1
-				//i am now less than U no matter what I do in M
-				less = true;
-			} else if (U[i] && !N[i]) {
-				//U[i]==1
-				//N[i]==0
-				//do nothing
-				//because it makes sense to leave M at 0 for lower values
-				//we are now less than U
-				less = true;
-			} else if (!U[i] && !N[i]) {
-				//U[i]==0
-				//N[i]==0
-				//have to check if it is definitely less, otherwise we can't flip
-				if (less) {
-					M[i].flip();
-				}
-			}
-		}
-
-		for (int i = 32; i >= 0; --i) {
-			//compare M with L to fix M to be greater than L
-			if (!L[i] && M[i]) {
-				//L[i]==0
-				//M[i]==1
-				//too big must flip bit
-				M[i].flip();
-			} else if (L[i] && M[i]) {
-				//L[i]==1
-				//M[i]==1
-				//same size
-				//do nothing
-				//no need to check for a flip here because we minimized before
-			} else if (!L[i] && !M[i]) {
-				//L[i]==0
-				//M[i]==0
-				//same size
-				//do nothing
-			} else if (L[i] && !M[i]) {
-				//L[i]==1
-				//M[i]==0
-				//i am definitely smaller
-				break;
-			}
-		}
-		cout << M.to_ulong() << endl;
-	}
+    ll N, L, U;
+    while (scanf("%lld %lld %lld", &N, &L, &U) == 3)
+        printf("%lld\n", solve(L, U, N));
+    return 0;
 }
