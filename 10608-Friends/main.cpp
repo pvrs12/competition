ed4bf015bfb7b3e3b1fd35504aa64a5b03da92d0
Patrick Vares <pvrs12@gmail.com>
2015-02-11 23:26:11 -0500

    still haven't made friends...

--- 10608-Friends/main.cpp
+++ 10608-Friends/main.cpp
@@ -3,20 +3,34 @@
 
 using namespace std;
 
-int findDepth(const vector<int>& djs, int root, int depth, vector<bool>& seen) {
+bool isCycle(const vector<int>& djs, int root,
+		vector<bool>* visited = nullptr) {
+
+	if (visited == nullptr) {
+		visited = new vector<bool>(djs.size(), false);
+	}
 	if (root == -1) {
-		return depth;
+		delete (visited);
+		return false;
 	}
-	if (seen[root]) {
-		return depth;
+	if (visited->at(root)) {
+		delete (visited);
+		return true;
 	}
-	seen[root] = true;
-	return findDepth(djs, djs[root], depth + 1, seen);
+	visited->at(root) = true;
+	return isCycle(djs, djs[root], visited);
 }
 
-int findDepth(const vector<int>& djs, int root) {
-	vector<bool> v(false, djs.size());
-	return findDepth(djs, djs[root], 0, v);
+int findRoot(const vector<int>& djs, int root) {
+	if (root == -1) {
+		return root;
+	}
+	int ret = findRoot(djs, djs[root]);
+	if (ret == -1) {
+		return root;
+	} else {
+		return ret;
+	}
 }
 
 int main() {
@@ -26,17 +40,25 @@ int main() {
 		int n, m;
 		cin >> n >> m;
 		//n citizens
+		//m friendships
 		vector<int> djs(n + 1, -1);
+		vector<int> rootCount(n + 1, 0);
 		int biggest = 0;
+		int cycle = 0;
 		for (int j = 0; j < m; ++j) {
 			int p1, p2;
 			cin >> p1 >> p2;
 			djs[p2] = p1;
-			int depth = findDepth(djs, p2);
-			if (depth > biggest) {
-				biggest = depth;
+			if (isCycle(djs, p2)) {
+				cycle++;
+				//undo what you just did
+				djs[p2] = -1;
 			}
+			int count = ++rootCount[findRoot(djs, p2)];
+			biggest = max(count, biggest);
 		}
+		biggest += (1 - cycle);
 		cout << biggest << endl;
 	}
+	return 0;
 }
